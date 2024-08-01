4bd8645304602959c2a7cb7d0415405205bd41e6
cuhrmunken <elliott.davis@crestonetech.com>
2024-07-31 20:53:08 -0700

    completes user controller methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ module.exports = {
       console.log(stubString);
 
       const userData = await User.find();
-
       res.json(userData);
     } catch (error) {
       res.status(400).json(`Get All Users failed: ${error}`);
@@ -18,7 +17,9 @@ module.exports = {
     try {
       const stubString = `reached function getUser(${req.params.userId}) at ${req.method} /api/users-${req.url}`;
       console.log(stubString);
-      res.json(stubString);
+
+      const userData = await User.findOne({ _id: req.params.userId });
+      res.status(200).json(userData);
     } catch (error) {
       res.status(400).json(`Get User failed: ${error}`);
     }
@@ -26,8 +27,10 @@ module.exports = {
   async deleteUser(req, res) {
     try {
       const stubString = `reached function deleteUser(${req.body.userId}) at ${req.method} /api/users${req.url}`;
+
       console.log(stubString);
-      res.json(stubString);
+      const userData = await User.deleteOne({ _id: req.params.userId });
+      res.status(200).json(userData);
     } catch (error) {
       res.status(400).json(`User deletion failed: ${error}`);
     }
@@ -36,18 +39,23 @@ module.exports = {
     try {
       const stubString = `reached function updateUser(${req.params.userId}) at ${req.method} /api/users${req.url} with username/email ${req.body.username}/${req.body.email}`;
       console.log(stubString);
-      res.json(stubString);
+
+      const userData = await User.updateOne(
+        { _id: req.params.userId },
+        req.body
+      );
+      res.status(200).json(userData);
     } catch (error) {
       res.status(400).json(`User update failed: ${error}`);
     }
   },
   async createUser(req, res) {
     try {
-      //   const stubString = `reached function createUser() at ${req.method} /api/users${req.url} with username/email ${req.body.username}/${req.body.email}`;
-      //   console.log(stubString);
+      const stubString = `reached function createUser() at ${req.method} /api/users${req.url} with username/email ${req.body.username}/${req.body.email}`;
+      console.log(stubString);
 
-      const newUser = await User.create(req.body);
-      res.status(200).json(newUser);
+      const userData = await User.create(req.body);
+      res.status(200).json(userData);
     } catch (error) {
       res.status(400).json(`User creation failed: ${error}`);
     }
