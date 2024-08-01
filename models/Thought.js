c6aff2b7324383d4afd91d4d1ffae05f7487643b
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-01 16:36:36 -0700

    adds Thought Model and completes basic API actions

---
+++ models/Thought.js
@@ -0,0 +1,65 @@
+const {
+  Schema,
+  model,
+  Types: { ObjectId },
+} = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: ObjectId,
+    default: new ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+});
+
+// Schema for Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: {
+      type: [reactionSchema],
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
